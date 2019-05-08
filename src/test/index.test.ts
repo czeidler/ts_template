e3114c31b10835aac1f0b1426fe6fcead8dcbbbf
Clemens Zeidler <clemens.zeidler@smartparking.com>
2019-05-08 14:29:53 +1200

    Init repo

---
+++ src/test/index.test.ts
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('template test', function () {
+  describe('test stub', () => {
+     it('nothing', async () => {
+      assert(true);
+    });
+  });
+});
